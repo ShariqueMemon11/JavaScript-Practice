bf8c876a4a16fdf59d1c97b266d1232e3918293d
Shariq.x <memonshariq10@gmail.com>
2024-10-30 21:27:06 +0500

    Math built-in object

---
+++ 5. Math Built-in object/index.js
@@ -0,0 +1,21 @@
+//math = built-in object that provides collection of properties and methods
+ 
+let x = 3.21;
+let y = 2;
+let z;
+
+//z=Math.round(x);
+//z=Math.floor(x);
+//z=Math.ceil(x);
+//z=Math.trunc(x);
+//z=Math.pow(x,y);
+//z=Math.sqrt(x);
+//z=Math.log(x);
+//z=Math.sin(x);
+//z=Math.cos(x);
+//z=Math.tan(x);
+//z=Math.abs(x);
+//z=Math.sign(x);
+//z=Math.max(x,y);
+z=Math.min(x,y);
+console.log(z);
