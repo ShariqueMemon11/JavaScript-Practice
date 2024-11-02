35ce830e1144ebc6bf6fcc1a0157637d73c7e81b
Shariq.x <memonshariq10@gmail.com>
2024-11-02 23:09:40 +0500

    switch statements

---
+++ 10. switch cases/index.js
@@ -0,0 +1,35 @@
+// swtich cases = can be used as a efficient replacement to many if else 
+// statements
+
+let day = `Tuesday`
+
+switch (day) {
+    case `Monday`:
+        console.log('today is Monday');
+        break;
+
+    case `Tuesday`:
+        console.log('today is tuesday');
+        break;
+    
+    case `wednesday`:
+        console.log('today is wednesday');
+        break;
+
+    case `Thusdaay`:
+        console.log('today is thursday')
+
+        break;
+    
+    case `Friday`:
+        console.log('today is friday')
+        break;
+
+    case `Saturday`:
+        console.log('today is satday')
+        break;
+    
+    case `sunday`:
+        console.log('today is sunday')
+        break;    
+}
