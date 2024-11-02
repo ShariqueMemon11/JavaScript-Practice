11f6bf78d57683e780cae5ae4795ea16efe20675
Shariq.x <memonshariq10@gmail.com>
2024-11-02 20:42:38 +0500

    ternary operator

---
+++ 9. ternary operation/index.js
@@ -0,0 +1,8 @@
+// ternary operator = a shortcut to if{} & else statements
+// helps to assign a veriable based on conditio
+
+let time = 9;
+
+let greatings = time < 12 ? `good Morning` : `Good Afternoon`
+
+console.log(greatings)
