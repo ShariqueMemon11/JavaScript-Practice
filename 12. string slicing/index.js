6abc3583329714dc0e4e35ea24c05c7074eb306f
Shariq.x <memonshariq10@gmail.com>
2024-11-05 23:29:00 +0500

    string slicing

---
+++ 12. string slicing/index.js
@@ -0,0 +1,9 @@
+//string slicing = creating a sub string from a part of another string 
+
+// string.slicing(start, end)
+
+let fullname = "shariq memon"
+ 
+let firstname = fullname.slice(0, 5)
+
+console.log(firstname)
