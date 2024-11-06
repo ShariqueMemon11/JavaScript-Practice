fc757a5bb97bb46314c899439ac54c2435de9e79
Shariq.x <memonshariq10@gmail.com>
2024-11-06 12:30:09 +0500

    string slicing

--- 12. string slicing/index.js
+++ 12. string slicing/index.js
@@ -4,6 +4,29 @@
 
 let fullname = "shariq memon"
  
-let firstname = fullname.slice(0, 5)
+//let firstname = fullname.slice(0, 5)
+ 
+//letlastname = fullname.slice(7, 12)
+
+
+let firstname = fullname.slice( 0 , fullname.indexOf(" "));
+
+let lastname = fullname.slice(fullname.indexOf(" ") + 1);
+
+let firstchar = fullname.slice(0,1);
+
+let lastchar = fullname.slice(-1);
+
+let email = "shariq@gmail.com";
+
+let username = email.slice( 0 , email.indexOf("@") );
 
 console.log(firstname)
+
+console.log(lastname) 
+
+console.log(firstchar) 
+
+console.log(lastchar)
+
+console.log(username)
