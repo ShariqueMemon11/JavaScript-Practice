8d284087030f0f783629d9f7ec94c3793fa382c9
Shariq.x <memonshariq10@gmail.com>
2024-11-06 13:06:08 +0500

    Logical operator

---
+++ 14 . logical operaters/index.js
@@ -0,0 +1,21 @@
+// logical operators = used to combine or manipulate boolean
+//                     values (true or false)
+
+//    And => &&
+//    or  => ||
+//    Not => !
+
+let temp = 19;
+let month = 'Nov'
+
+if (temp!=20) {
+    console.log(`temp is cold`)
+}
+
+if (temp == 20 || temp == 21) {
+    console.log(`its hot outside`)
+}
+
+if (temp == 19 && month == "Nov") {
+    console.log(`yes its cold outside bcz its Nov`)
+}
