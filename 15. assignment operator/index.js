b02c40b6aa9f07c57d348e01d187599bf3a2d789
Shariq.x <memonshariq10@gmail.com>
2024-11-10 20:21:57 +0500

    assignment operator

---
+++ 15. assignment operator/index.js
@@ -0,0 +1,43 @@
+// = Assignment operator
+
+const PI = 3.14;
+
+// == comparison (compare if values are equal)
+
+if (PI == 3.14) {
+   console.log("that is PI");
+}
+else
+{
+    console.log("that is not PI")
+}
+
+// === strict equality operator (compare if values & datatype are equal)
+
+const age = 21;
+
+if (age === '21') {
+    console.log('data type and values are equal')
+}
+else{
+    console.log('data type and value is not equal')
+}
+
+// != inequality operator 
+
+if (age != '22') {
+    console.log('xIt is not PI')
+}
+else{
+    console.log('It is PI')
+}
+
+// !== strict inequality operator 
+
+if (age !== '21') {
+    console.log('It is a PI')
+}
+else
+{
+    console.log('It is a not PI')
+}
